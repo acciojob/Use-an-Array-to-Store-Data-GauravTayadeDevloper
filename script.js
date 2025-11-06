51e10424325f0853dca8855e03b0bfbf0b383588
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-11-06 17:17:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 
+// Create an array with at least 5 elements containing a string, number, and boolean
+let yourArray = ["Gaurav", 25, true, "JavaScript", 3.14];
+
+// Display the array (optional)
+console.log(yourArray);
